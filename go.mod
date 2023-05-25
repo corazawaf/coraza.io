c3be34a72283128c1d42ff6bc889084cea687397
José Carlos Chávez <jcchavezs@gmail.com>
2023-05-25 11:57:10 +0200

    chore: adds directives doc generator. (#167)
    * chore: adds directives doc generator.
    
    * chore: adds generate command.
    
    * docs: adds more directive.
    
    * chore: commit without sec files to fix case-sensitiviness.
    
    * chore: readds sec generated files.
    
    * chore: adds comment for generated content files.
    
    * docs: update generated docs.
    
    * chore: uses go mod vendor instead of submodules.
    
    * feat: tweaks autogeneration by skipping those files whose only change was the lastmod date.
    
    Currently, running the generator would end up in modifying files that did not get modified at all, just the regenerated with a different lastmod date. With this change we skip those files and only modify those that have truly more changes.
    
    * chore: moves to a single page for directives.

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/corazawaf/coraza.io
+
+go 1.19
+
+require (
+	github.com/corazawaf/coraza/v3 v3.0.0-rc.2
+	github.com/magefile/mage v1.14.0
+)
+
+require (
+	github.com/corazawaf/libinjection-go v0.1.2 // indirect
+	github.com/petar-dambovaliev/aho-corasick v0.0.0-20211021192214-5ab2d9280aa9 // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	rsc.io/binaryregexp v0.2.0 // indirect
+)
---
+++ tools/directivesgen/main.go
@@ -0,0 +1,169 @@
+// Copyright 2023 The OWASP Coraza contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"bufio"
+	"bytes"
+	_ "embed"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"html"
+	"html/template"
+	"log"
+	"os"
+	"path"
+	"regexp"
+	"sort"
+	"strings"
+	"time"
+)
+
+type Page struct {
+	LastModification string
+	Directives       []Directive
+}
+
+type Directive struct {
+	Name             string
+	Description      string
+	Syntax           string
+	Default          string
+	Date             string
+	LastModification string
+	Content          string
+}
+
+//go:embed template.md
+var contentTemplate string
+
+const dstFile = "./content/docs/seclang/directives.md"
+
+func main() {
+	tmpl, err := template.New("directive").Parse(contentTemplate)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filepath := path.Join(os.Args[1], "internal/seclang/directives.go")
+	src, err := os.ReadFile(filepath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "directives.go", src, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dstf, err := os.Create(dstFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dstf.Close()
+
+	page := Page{
+		LastModification: time.Now().Format(time.RFC3339),
+	}
+
+	ast.Inspect(f, func(n ast.Node) bool {
+		switch fn := n.(type) {
+
+		// catching all function declarations
+		// other intersting things to catch FuncLit and FuncType
+		case *ast.FuncDecl:
+			fnName := fn.Name.String()
+			if !strings.HasPrefix(fnName, "directive") {
+				return true
+			}
+
+			if fn.Doc == nil {
+				return true
+			}
+
+			directiveName := fnName[9:]
+
+			page.Directives = append(page.Directives, parseDirective(directiveName, fn.Doc.Text()))
+		}
+		return true
+	})
+
+	sort.Slice(page.Directives, func(i, j int) bool {
+		return page.Directives[i].Name < page.Directives[j].Name
+	})
+
+	content := bytes.Buffer{}
+	err = tmpl.Execute(&content, page)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = dstf.WriteString(html.UnescapeString(content.String()))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func parseDirective(name string, doc string) Directive {
+	d := Directive{
+		Name: name,
+	}
+
+	fieldAppenders := map[string]func(d *Directive, value string){
+		"Description": func(d *Directive, value string) { d.Description += value },
+		"Syntax":      func(d *Directive, value string) { d.Syntax += value },
+		"Default":     func(d *Directive, value string) { d.Default += value },
+	}
+
+	previousKey := ""
+	scanner := bufio.NewScanner(strings.NewReader(doc))
+	for scanner.Scan() {
+		if strings.HasPrefix(scanner.Text(), "directive") {
+			continue
+		}
+
+		if len(strings.TrimSpace(scanner.Text())) == 0 {
+			continue
+		}
+
+		if strings.HasPrefix(scanner.Text(), "---") {
+			break
+		}
+
+		key, value, ok := strings.Cut(scanner.Text(), ": ")
+		if !ok {
+			key = previousKey
+			value = " " + scanner.Text()
+		}
+
+		if fn, ok := fieldAppenders[key]; ok {
+			fn(&d, value)
+			previousKey = key
+		} else if previousKey != "" {
+			fieldAppenders[previousKey](&d, value)
+		} else {
+			log.Fatalf("unknown field %q", key)
+		}
+	}
+
+	for scanner.Scan() {
+		d.Content += decorateNote(scanner.Text()) + "\n"
+	}
+
+	d.Description = addsLinksToDirectives(d.Description)
+	d.Content = addsLinksToDirectives(d.Content)
+
+	return d
+}
+
+func decorateNote(s string) string {
+	return strings.Replace(s, "Note:", "**Note:**", -1)
+}
+
+func addsLinksToDirectives(s string) string {
+	var re = regexp.MustCompile("(`Sec[a-zA-Z0-9]+`)")
+	return re.ReplaceAllString(s, `[$1](#$1)`)
+}
