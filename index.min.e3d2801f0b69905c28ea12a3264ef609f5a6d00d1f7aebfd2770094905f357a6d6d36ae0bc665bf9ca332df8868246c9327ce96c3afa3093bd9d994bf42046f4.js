d7723fe38166c27896cefb862d679622cc1dde88
fzipi <fzipi@users.noreply.github.com>
2026-01-15 11:51:07 +0000

    deploy: f50db9d997fc4d0490a6cd819a51d6fb0bf651ba

--- index.min.e3d2801f0b69905c28ea12a3264ef609f5a6d00d1f7aebfd2770094905f357a6d6d36ae0bc665bf9ca332df8868246c9327ce96c3afa3093bd9d994bf42046f4.js
+++ index.min.e3d2801f0b69905c28ea12a3264ef609f5a6d00d1f7aebfd2770094905f357a6d6d36ae0bc665bf9ca332df8868246c9327ce96c3afa3093bd9d994bf42046f4.js
@@ -42,7 +42,6 @@ Default: Native
 SecAuditLogParts # Description: Defines which parts of each transaction are going to be recorded in the audit log. Each part is assigned a single letter; when a letter appears in the list then the equivalent part will be recorded. See below for the list of all parts.
 Syntax: SecAuditLogParts [PARTLETTERS]
 Default: ABCFHZ
-The format of the audit log format is documented in detail in the Audit Log Data Format Documentation.
 Example:
 SecAuditLogParts ABCFHZ Available audit log parts:
 A: Audit log header (mandatory). B: Request headers. C: Request body (present only if the request body exists and Coraza is configured to intercept it. This would require SecRequestBodyAccess to be set to on). D: Reserved for intermediary response headers; not implemented yet. E: Intermediary response body (present only if Coraza is configured to intercept response bodies, and if the audit log engine is configured to record it. Intercepting response bodies requires SecResponseBodyAccess to be enabled). Intermediary response body is the same as the actual response body unless Coraza intercepts the intermediary response body, in which case the actual response body will contain the error message. F: Final response headers. G: Reserved for the actual response body; not implemented yet. H: Audit log trailer. I: This part is a replacement for part C. It will log the same data as C in all cases except when multipart/form-data encoding in used. In this case, it will log a fake application/x-www-form-urlencoded body that contains the information about parameters but not about the files. This is handy if you don’t want to have (often large) files stored in your audit logs. J: This part contains information about the files uploaded using multipart/form-data encoding. K: This part contains a full list of every rule that matched (one per line) in the order they were matched. The rules are fully qualified and will thus show inherited actions and default operators. Z: Final boundary, signifies the end of the entry (mandatory). SecAuditLogRelevantStatus # Description: Configures which response status code is to be considered relevant for the purpose of audit logging.
@@ -114,7 +113,10 @@ Syntax: SecRuleRemoveByTag [TAG]
 Normally, you would use SecRuleRemoveById to remove rules, but it may occasionally be easier to disable an entire group of rules with SecRuleRemoveByTag. Matching is by case-sensitive string equality.
 Example:
 SecRuleRemoveByTag attack-dos Note: OWASP CRS has a list of supported tags https://coreruleset.org/docs/rules/metadata/
-SecRuleUpdateTargetByID # Description: Updates the target (variable) list of the specified rule(s).
+SecRuleUpdateActionByID # Description: Updates the action list of the specified rule(s).
+Syntax: SecRuleUpdateActionById ID ACTIONLIST
+This directive will overwrite the action list of the specified rule with the actions provided in the second parameter. It has two limitations: it cannot be used to change the ID or phase of a rule. Only the actions that can appear only once are overwritten. The actions that are allowed to appear multiple times in a list, will be appended to the end of the list. The following example demonstrates how SecRuleUpdateActionById is used:
+SecRuleUpdateActionById 12345 &quot;deny,status:403&quot; SecRuleUpdateTargetByID # Description: Updates the target (variable) list of the specified rule(s).
 Syntax: SecRuleUpdateTargetById ID TARGET1[|TARGET2|TARGET3]
 This directive will append variables to the specified rule with the targets provided in the second parameter. The rule ID can be single IDs or ranges of IDs. The targets are separated by a pipe character.
 SecRuleUpdateTargetByTag # Description: Updates the target (variable) list of the specified rule(s) by tag.
